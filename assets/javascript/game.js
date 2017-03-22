b1d54ac88a12bbd859202c35b93a8fe00f8e46ef
Sharon Oud <mrh_sharon@hotmail.com>
2017-03-21 21:19:19 -0500

    Useless update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var startNumber = 0;
 var crystal1 = Math.floor(Math.random() * (12 - 1) + 1);
 var crystal2 = Math.floor(Math.random() * (12 - 1) + 1);
 var crystal3 = Math.floor(Math.random() * (12 - 1) + 1);
-var crystal4 = Math.floor(Math.random() * (12 - 1) + 1);
+var crystal4 = Math.floor(Math.random() * (12 - 1) + 1);                          
 var targetNumber= Math.floor(Math.random() * (120 - 19) + 19);
 var win;
 var lose;
@@ -17,10 +17,23 @@ console.log(targetNumber, crystal1,crystal2,crystal3,crystal4 );
 //function when the user clicks crystal 1 they get the randomly assigned points added to their running total
 //var startNumber = 0;
 $('button').click(function (currentvalue){
-	alert("CLICKED");
-   startNumber = Number(startNumber) + Number($(this).val());
-    $('.total').text("Total: "+startNumber);        
+	console.log("CLICKED"+" " + crystal1);
+   total = startNumber +  crystal1;
+   console.log(total);
+    $('.total').text("Total: "+startNumber);   
+    document.getElementById("total").innerHTML = total;     
 });
+
+
+
+
+
+
+
+
+
+
+
 
 //$('.total').text(startNumber); 
 
@@ -30,7 +43,7 @@ $('button').click(function (currentvalue){
 
 
 
-//document.getElementById("greencounter").innerHTML = theNewValue;
+//
 //parseInt()
 // = n === ()
 // = n <||>()
